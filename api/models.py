407f7a5b62cf0984a7e677ac02d358f434b620ec
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-24 14:59:45 +0500

    models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
