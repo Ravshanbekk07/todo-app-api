00695c7091bc55eb6ed30499f35f8734cab1b5c4
Ravshanbekk07 <ramazonof07@gmail.com>
2023-07-26 16:40:48 +0500

    task views

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2&*^lz1_@1z91y%g0xrol%&%kl88uf^2$*1p3m3&58*#g#_&()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
