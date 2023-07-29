9ff862a679b123b396a01528800637a224ca5837
Ravshanbekk07 <ramazonof07@gmail.com>
2023-07-29 08:54:08 +0500

    static

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Task
 # Register your models here.
-admin.site.register(Task)
+
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id','title','description','completed','created_at','updated_at','author')
+    list_display_links= ('id','title','created_at')
+    list_filter = ("completed",'created_at','updated_at')
+    search_fields = ('id','title','description')
+    ordering = ("-created_at","-updated_at")
+    date_hierarchy = 'created_at'
+
+    def author(self,obj):
+        return obj.author.username
